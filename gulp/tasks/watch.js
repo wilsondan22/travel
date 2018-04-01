3837289b9f5578705c0e9e8fd078996eea70774c
Daniel Wilson <wilsondan22@gmail.com>
2018-03-31 20:01:19 -0500

    reset to sprites

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,21 +3,25 @@ watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
-	browserSync.init({
-		notify: false, 
-		server: {
-			baseDir: "app"
-		}
-	});
-	watch('./app/index.html', function() {
-		browserSync.reload();
-	});
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('cssInject');
-	});
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
 });
 
 gulp.task('cssInject', ['styles'], function() {
-	return gulp.src('./app/temp/styles/styles.css')
-	.pipe(browserSync.stream());
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,13 @@
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
+require('./gulp/tasks/sprites');
+
+
+
+
+
+
+
+
+
 
